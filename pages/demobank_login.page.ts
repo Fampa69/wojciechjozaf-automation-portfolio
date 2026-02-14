3414b690a160d446cd851b8044d5ebff0a344dc0
Fampa69 <wojtekjozaf@gmail.com>
2026-02-14 18:20:35 +0100

    Refactoring of transfer test cases, changing names of pages files, and deletion of redundant classes attribiutes

--- pages/demobank_login.page.ts
+++ pages/demobank_login.page.ts
@@ -2,12 +2,10 @@ import { Page, Locator } from '@playwright/test';
 
 export class LoginPage{
 
-    readonly page: Page;
     readonly usernameBox: Locator;
     readonly passwordBox: Locator;
 
     constructor(page: Page){
-        this.page = page;
         this.usernameBox = page.getByTestId('login-input');
         this.passwordBox = page.getByTestId('password-input');
     }
