2ef85b4c0d56c8a6b34c85fa88bbde1f497ce248
Fampa69 <wojtekjozaf@gmail.com>
2026-02-22 12:46:05 +0100

    addition of API test suit test cases, addition of mocked .json file and some small changes

--- tests/my_tests/api_testing/basicApiTesting.spec.ts
+++ tests/my_tests/api_testing/basicApiTesting.spec.ts
@@ -1,19 +1,42 @@
 import { test, expect } from '@playwright/test';
+import mockEntries from '../../../mockdata/demoblazeGetEntries.json';
 
 test.describe("Positive Route for API Testing", () => {
+    //arrange
+    const Url = "https://www.demoblaze.com/index.html";
 
     test("It should display products on the website", async ({ page }) => {
         //arrange
-        const Url = "https://www.demoblaze.com/index.html";
         const response = await page.waitForResponse(res => res.url().endsWith('/entries'));
-        await page.goto(Url);
         const product = page.locator(".card.h-100");
         const body = await response.json();
+        
+        //act
+        await page.goto(Url);
 
         //assert
         expect(response.status()).toBe(200);
         expect(body.Items).not.toHaveLength(0);
         await expect(product).not.toHaveCount(0);
+    })
+
+    test("It should display mocked products on the website", async ({ page }) => {
+        //arrange
+        await page.route("**/entries", async (route) => {
+            await route.fulfill({
+                status: 200,
+                contentType: "application/json",
+                json: mockEntries
+            });
+        });
+        
+        //act
+        await page.goto(Url);
+        await page.pause();
+
+        //assert
+        await expect(page.getByText('Super Mocked Phone ProXL 10')).toBeVisible();
+        await expect(page.getByText('TPhone 17 Pro')).toBeVisible();
     })
 })
 
